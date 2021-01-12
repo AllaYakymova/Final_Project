e2e7fb85b25a98d93d6d56364c120c1a77935347
Allegra <allochkaja112@gmail.com>
2021-01-12 23:22:49 +0200

    Redux #1

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -20,12 +20,16 @@ const Cart = () => {
   })
 
   const cartTotalBlockMobile = (<span className='cart__total-block-mobile'><h2 className="cart__text cart__text-title">SHOPPING BAG TOTAL</h2><div className="cart__total"><form noValidate autoComplete="off" className='mb3'><p className="cart__text_12 mb1">ADD A DISCOUNT CODE</p><TextField id="standard-basic" label="" /></form><div className='cart__info-item'><p className="cart__text cart__text-subtitle">ORDER VALUE:</p><p className="cart__text cart__text-subtitle">{totalSum} &#36;</p></div><div className='cart__info-item'><p className="cart__text cart__text-subtitle">DELIVERY</p><p className="cart__text cart__text-subtitle">free</p></div><div className='cart__info-item'><p className="cart__text cart__text-title">TOTAL</p><p className="cart__text cart__text-title_18">{totalSum} &#36;</p></div><Button text='CHECKOUT' isBlack size26357 fz18 onClick={() => {}}/></div></span>)
+
   const cartTotalBlockDesktop = (<div className="cart__total cart__total-block-desktop"><h2 className="cart__text cart__text-title">SHOPPING BAG TOTAL</h2><form noValidate autoComplete="off" className='mb3 cart__total-line'><p className="cart__text_12 mb1 ">ADD A DISCOUNT CODE</p><TextField id="standard-basic" label="" /></form><div className='cart__info-item'><p className="cart__text cart__text-subtitle">ORDER VALUE:</p><p className="cart__text cart__text-subtitle ">{totalSum} &#36;</p></div><div className='cart__info-item'><p className="cart__text cart__text-subtitle">DELIVERY</p><p className="cart__text cart__text-subtitle">free</p></div><div className='cart__info-item'><p className="cart__text cart__text-title">TOTAL</p><p className="cart__text cart__text-title_18">{totalSum} &#36;</p></div><Button text='CHECKOUT' isBlack size26357 fz18 onClick={() => {}}/></div>)
 
   const shopButton = (<span className='cart__shop-button'><Button text='KEEP SHOPPING' isBlack size26957 fz18 onClick={() => console.log('test-back')}/></span>)
 
+  const paginatorLeft = <div><div className="paginator"/></div>
+
   return (
     <div className="cart-wrap">
+      {paginatorLeft}
       <h3 className="cart__text-title-phone">Shopping bag</h3>
       {shopButton}
       <div className="cart">
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,5 +1,36 @@
 import {combineReducers} from 'redux';
+import { REQUEST_PRODUCTS_SUCCESS, REQUEST_PRODUCTS_ERROR, REQUEST_PRODUCTS_IN_PROCESS } from './../constants/constants';
+
+const initialStore = {
+  products: [],
+  isLoaded: false,
+  error: '',
+};
+
+const getProductsReducer = (store = initialStore, action) => {
+  switch (action.type) {
+    case REQUEST_PRODUCTS_SUCCESS:
+      return {
+        ...store,
+        isLoaded: true,
+        products: action.payload,
+      };
+    case REQUEST_PRODUCTS_ERROR:
+      return {
+        ...store,
+        isLoaded: false,
+        error: action.payload
+      };
+    case REQUEST_PRODUCTS_IN_PROCESS:
+      return {
+        ...store,
+        isLoaded: false,
+      };
+    default:
+      return store;
+  }
+};
 
 export const rootReducer = combineReducers({
-
+  getProducts: getProductsReducer
 });
